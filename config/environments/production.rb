9c81422f3ed0c0509e5f40e5275d9497251c76cf
Loki Ng <loki@lokinote.com>
2012-02-10 16:07:18 +0800

    use asset pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,8 +28,19 @@ Enki::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Disable Rails's static asset server
-  # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+  config.serve_static_assets = true
+
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
+  
+  # using yui-compressor to compress css
+  config.assets.css_compressor = :yui
+
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
